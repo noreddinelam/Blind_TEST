ea751485a7e675650deb33c7173833045d480e0b
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2022-01-06 00:30:04 +0100

    initial commit

---
+++ src/main/java/com/example/blind_test/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.blind_test;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
