ddcadf610557640a031b024405224aeecc3f3cc1
IDRIS ABDELLAH <50142372+abdellah-idris@users.noreply.github.com>
2022-01-06 15:52:42 +0100

    [database] implement game requests

--- src/main/java/com/example/blind_test/database/SQLStatements.java
+++ src/main/java/com/example/blind_test/database/SQLStatements.java
@@ -4,7 +4,20 @@ import static com.example.blind_test.database.SQLTablesInformation.*;
 
 public class SQLStatements {
 
+    public static final String LIST_OF_GAME_NOT_STARTED = "SELECT * FROM"
+            + GAME_TABLE + " WHERE " + GAME_STATE + "=0 ;";
+
+    public static final String CHANGE_GAME_STATE = "UPDATE " + GAME_TABLE + " SET " + GAME_STATE + "= ?" +
+            " WHERE " + GAME_ID + "= ?; ";
+
+    public static final String ID_OF_CURRENT_QUESTION = "UPDATE " + GAME_TABLE + " SET "
+            + GAME_CURRENT_QUESTION + " = ? " +
+            " WHERE " + GAME_ID + "= ?; ";
+
+
     private SQLStatements() {
     }
+
+
 }
 
--- src/main/java/com/example/blind_test/database/SQLTablesInformation.java
+++ src/main/java/com/example/blind_test/database/SQLTablesInformation.java
@@ -5,5 +5,14 @@ public class SQLTablesInformation {
     public static final String PLAYER_TABLE = "PLAYER_TABLE";
     public static final String PLAYER_USERNAME_COLUMN = "username";
 
+    public static final String GAME_TABLE = "Game";
+    public static final  String GAME_ID="id";
+    public static final  String GAME_TYPE="type";
+    public static final  String GAME_CURRENT_QUESTION="current_question";
+    public static final  String GAME_ROUNDS="rounds";
+    public static final  String GAME_PLAYERS="players";
+    public static final  String GAME_TIME_QUESTION="timeQuestion";
+    public static final  String GAME_STATE="state";
+
     private SQLTablesInformation(){}
 }
