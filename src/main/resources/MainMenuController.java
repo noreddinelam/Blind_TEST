8d14fec045ca6b4b0693166f5ec85677cd1cc3b6
arthurgmt <arthur.guillaumot@gmail.com>
2022-01-06 13:31:00 +0100

    modify ressources

---
+++ src/main/resources/MainMenuController.java
@@ -0,0 +1,32 @@
+package front;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class CreateGameMenuController {
+
+    ObservableList<String> numberOfPlayersList = FXCollections.observableArrayList("2","3","4","5","6","7","8");
+    ObservableList<String> numberOfQuestionsList = FXCollections.observableArrayList("10","20","30","40");
+    ObservableList<String> responseTimeList = FXCollections.observableArrayList("5","10","15");
+
+
+    @FXML
+    private ChoiceBox<String> numberOfPlayers;
+
+    @FXML
+    private ChoiceBox<String> numberOfQuestions;
+
+    @FXML
+    private ChoiceBox<String> responseTime;
+
+    @FXML
+    private void initialize(){
+        numberOfPlayers.setItems(numberOfPlayersList);
+        numberOfQuestions.setItems(numberOfQuestionsList);
+        responseTime.setItems(responseTimeList);
+    }
+
+
+}
