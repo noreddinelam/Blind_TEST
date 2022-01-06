b48cacd97ce82a78156fb3bb9aea72c523093363
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2022-01-06 23:40:34 +0100

    [Controller] start controllers

--- src/main/java/com/example/blind_test/front/controllers/MainMenuController.java
+++ src/main/java/com/example/blind_test/front/controllers/MainMenuController.java
@@ -2,27 +2,61 @@ package com.example.blind_test.front.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
 
 public class MainMenuController {
 
-    ObservableList<String> numberOfPlayersList = FXCollections.observableArrayList("2","3","4","5","6","7","8");
-    ObservableList<String> numberOfQuestionsList = FXCollections.observableArrayList("10","20","30","40");
-    ObservableList<String> responseTimeList = FXCollections.observableArrayList("5","10","15");
-
-
-    @FXML
-    private ChoiceBox<String> numberOfPlayers;
+    ObservableList<Integer> numberOfPlayersList = FXCollections.observableArrayList(1, 2, 3, 4, 5);
+    ObservableList<Integer> numberOfQuestionsList = FXCollections.observableArrayList(5, 10, 15, 20);
+    ObservableList<Integer> responseTimeList = FXCollections.observableArrayList(10, 15, 20);
 
     @FXML
-    private ChoiceBox<String> numberOfQuestions;
+    private Button createAudioGameButton;
 
     @FXML
-    private ChoiceBox<String> responseTime;
+    private Button createImageGameButton;
 
     @FXML
-    private void initialize(){
+    private Button joinGameButton;
+
+    @FXML
+    private ListView<?> listOfGameToJoin;
+
+    @FXML
+    private ChoiceBox<Integer> numberOfPlayers;
+
+    @FXML
+    private ChoiceBox<Integer> numberOfQuestions;
+
+    @FXML
+    private ChoiceBox<Integer> responseTime;
+
+    @FXML
+    void onCreateAudioGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCreateImageGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onJoinGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onQuitGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void initialize() {
         numberOfPlayers.setItems(numberOfPlayersList);
         numberOfQuestions.setItems(numberOfQuestionsList);
         responseTime.setItems(responseTimeList);
