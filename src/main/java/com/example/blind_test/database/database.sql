013081da721a5bc1f4a1b312c44f80db1579700c
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2022-01-06 13:51:34 +0100

    [DB] update db tables

--- src/main/java/com/example/blind_test/database/database.sql
+++ src/main/java/com/example/blind_test/database/database.sql
@@ -10,11 +10,25 @@ use blind_test;
 CREATE TABLE `Question`
 (
     `id`           integer NOT NULL ,
+    `id_resource` varchar(255) NOT NULL ,
+    `response`   varchar(255) NOT NULL ,
+    `choice1`    varchar(255) NOT NULL ,
+    `choice2`    varchar(255) NOT NULL ,
+    `choice3`    varchar(255) NOT NULL ,
+
+    PRIMARY KEY (`id`)
+);
+
+-- ************************************** `Question_Game`
+
+CREATE TABLE `Question_Game`
+(
+    `id_question`  integer NOT NULL ,
     `id_game`    integer NOT NULL ,
-    `id_resource` varchar(45) NOT NULL ,
     `order`        integer NOT NULL ,
 
-    PRIMARY KEY (`id`),
+    KEY `FK_30` (`id_question`),
+    CONSTRAINT `FK_32` FOREIGN KEY `FK_30` (`id_question`) REFERENCES `Question` (`id`),
     KEY `FK_28` (`id_game`),
     CONSTRAINT `FK_26` FOREIGN KEY `FK_28` (`id_game`) REFERENCES `Game` (`id`)
 );
@@ -28,6 +42,7 @@ CREATE TABLE `Game`
     `current_question` integer NOT NULL ,
     `rounds`           integer NOT NULL ,
     `players`          integer NOT NULL ,
+    `timeQuestion`     timestamp NOT NULL ,
     `state`             bit NOT NULL ,
 
     PRIMARY KEY (`id`),
