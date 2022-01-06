4105f3ea8772c27b272f3dbafbbe78cbf7bf4c1c
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2022-01-06 13:56:49 +0100

    [DB] start requests implementation

--- src/main/java/com/example/blind_test/database/SQLTablesInformation.java
+++ src/main/java/com/example/blind_test/database/SQLTablesInformation.java
@@ -3,6 +3,7 @@ package com.example.blind_test.database;
 public class SQLTablesInformation {
 
     public static final String PLAYER_TABLE = "PLAYER_TABLE";
+    public static final String PLAYER_USERNAME_COLUMN = "username";
 
     private SQLTablesInformation(){}
 }
