c58bb887735431e50aca25880389af5b2f71246f
arthurgmt <arthur.guillaumot@gmail.com>
2022-01-07 18:30:29 +0100

    Update SQLStatements.java

--- src/main/java/com/example/blind_test/database/SQLStatements.java
+++ src/main/java/com/example/blind_test/database/SQLStatements.java
@@ -52,8 +52,6 @@ public class SQLStatements {
     public static final String DELETE_ALL_PLAYER_FOR_GAME =
             "DELETE FROM " + PLAYER_TABLE
                     + " WHERE " + PLAYER_ID_GAME + "=? ;";
-    public static final String GET_ALL_PLAYERS_OF_GAME =
-            "SELECT * FROM " + PLAYER_TABLE + " WHERE " + PLAYER_ID_GAME + "=? ;";
     public static String CREATE_PLAYER =
             "INSERT INTO " + PLAYER_TABLE
                     + "(" + PLAYER_USERNAME + "," + PLAYER_ID_GAME + "," + PLAYER_SCORE
