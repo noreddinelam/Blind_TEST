199f2b0bcf9eada65b58bc34b9669128dcbaa0de
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2022-01-07 01:39:54 +0100

    [ClientImpl] start client implementation functions

--- src/main/java/com/example/blind_test/front/controllers/MainMenuController.java
+++ src/main/java/com/example/blind_test/front/controllers/MainMenuController.java
@@ -38,7 +38,7 @@ public class MainMenuController extends Controller{
 
     @FXML
     void onCreateAudioGame(ActionEvent event) {
-        //this.clientImpl.createGame()
+        this.clientImpl.createGame(numberOfPlayers.getValue(),numberOfQuestions.getValue(),responseTime.getValue());
     }
 
     @FXML
