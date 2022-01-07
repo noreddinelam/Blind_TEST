fd8b10bcefda6e21b0487a6ccb53f38f23c02762
arthurgmt <arthur.guillaumot@gmail.com>
2022-01-07 22:25:03 +0100

    Update SQLStatements.java

--- src/main/java/com/example/blind_test/database/SQLStatements.java
+++ src/main/java/com/example/blind_test/database/SQLStatements.java
@@ -59,10 +59,10 @@ public class SQLStatements {
                     " VALUES (?,?,0) ;";
 
     public static final String CHANGE_QUESTION_STATE = "UPDATE "+ QUESTION_GAME_TABLE + " SET "+QUESTION_GAME_STATE +
-            " = 1 WHERE " + QUESTION_ID + " =? ;";
+            " = 1 WHERE " + QUESTION_GAME_ID_QUESTION + " = ? ;";
 
-    public static final String VERIFY_QUESTION_STATE = "SELECT "+  QUESTION_GAME_STATE + " FROM "+ QUESTION_TABLE +
-            "  WHERE " + QUESTION_ID + " =? ;";
+    public static final String VERIFY_QUESTION_STATE = "SELECT "+  QUESTION_GAME_STATE + " FROM "+ QUESTION_GAME_TABLE +
+            "  WHERE " + QUESTION_ID + " = ? ;";
 
     private SQLStatements() {
     }
